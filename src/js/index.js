4b80e703f95b64b29532dcf71aabb39f6833290d
Arver1 <aactepanov@mail.ru>
2018-11-07 01:32:00 +0300

    start to work over first slide

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import { landSlider } from './sliders/landSlider';
 import { vSlider } from './sliders/verticalSlider';
 import '../scss/style.scss';
 
-const landContainer = document.querySelector('.slideH_slider');
-const landSlides = landContainer.querySelectorAll('.slideH');
+const landContainer = document.querySelector('.sliderH');
+const landSlides = landContainer.querySelectorAll('.sliderH__slide');
 const fieldRange = document.querySelector('.sliderH__range');
 
 vSlider(document.querySelectorAll('.sliderV__slide'),fieldRange, 300);
