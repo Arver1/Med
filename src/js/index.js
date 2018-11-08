d6c915ba307daec34aa7ccc1db67473a372f8740
Arver1 <aactepanov@mail.ru>
2018-11-08 21:12:52 +0300

    add btn down

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,15 @@ window.addEventListener('load', () => {
   const landContainer = document.querySelector('.sliderH');
   const landSlides = landContainer.querySelectorAll('.sliderH__slide');
   const thumb = document.querySelector('.range__thumb');
+  const btndown = document.querySelector('.sliderV__sun-text');
 
   vSlider(document.querySelectorAll('.sliderV__slide'), 300);
   landSlider(landContainer, landSlides, 300, thumb);
+  btndown.addEventListener('click',()=>{
+    const event = new WheelEvent('wheel', {
+      deltaY: 100
+    });
+    window.dispatchEvent(event);
+  });
 });
 
